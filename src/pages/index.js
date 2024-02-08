7992751fb9d87d9284bdd81d926787fbbd950584
salik03 <uddinsalik03@gmail.com>
2024-02-09 00:08:13 +0530

    register here

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="https://salik.live">
+            to="https://gdsc.community.dev/events/details/developer-student-clubs-bennett-university-greater-noida-presents-build-it-yourself/">
             Register Here!
           </Link>
         </div>
